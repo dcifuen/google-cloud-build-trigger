176578949447e49d80e1d2dfc94e284ce32fb196
David Cifuentes <david.cifuentes@zalando.de>
2019-05-07 07:52:45 +0200

    Initial commit with code and config

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # google-cloud-build-trigger
-Run a Google Cloud Build trigger
+Firebase function that runs a Google Cloud Build trigger.
+
+Retrieve your trigger ID from API Explorer:
+https://cloud.google.com/cloud-build/docs/api/reference/rest/v1/projects.triggers/list
+
+```bash
+firebase functions:config:set cloudbuild.trigger_id="<TRIGGER_ID>"
+npm run deploy
+```
---
+++ functions/index.js
@@ -0,0 +1,46 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const rp = require('request-promise');
+
+const TRIGGER_ID = functions.config().cloudbuild.trigger_id;
+
+admin.initializeApp();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ return admin.credential.applicationDefault().getAccessToken()
+     .then(accessTokenObj => {
+      return accessTokenObj.access_token;
+     })
+     .then(accessToken => {
+      return runTrigger(accessToken);
+     })
+     .then(result => {
+      console.info(result);
+      response.send("OK");
+      return null;
+     })
+     .catch(error => {
+      console.error(error);
+      return null;
+     })
+});
+
+function runTrigger(accessToken) {
+ const options = {
+  method: 'POST',
+  uri: `https://cloudbuild.googleapis.com/v1/projects/${process.env.GCLOUD_PROJECT}/triggers/${TRIGGER_ID}:run`,
+  body: {
+   'branchName': 'master'
+  },
+  headers: {
+   Authorization: 'Bearer ' + accessToken
+  },
+  json: true
+ };
+ return rp(options).then(resp => {
+  return Promise.resolve(resp);
+ }).catch(err => {
+  console.error(err);
+  return Promise.resolve(null);
+ });
+}
